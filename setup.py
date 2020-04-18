3e1a75851a7db312eebd7d17224656499ff4a438
Jay Spendlove <jayclark24@gmail.com>
2020-04-17 17:16:33 -0700

    edit .travis.yml

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(name='gblearn',
         'Operating System :: POSIX::Linux',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     )
